cbbe0e0c4144cd422229be1c157b9b364e9994df
hexavhobia <rvx9forces@gmail.com>
2024-05-29 10:28:27 +0700

    fix: perbaiki UI Pendaftar

--- src/pages/data-pendaftar/cetak-bukti-pendaftaran.tsx
+++ src/pages/data-pendaftar/cetak-bukti-pendaftaran.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState, useEffect } from 'react'
-import ReactToPrint from 'react-to-print'
+import { useRef } from 'react'
+import { useReactToPrint } from 'react-to-print'
 import dayjs from 'dayjs'
 import { enumVerifikasi } from '@/libs/enum/enum-verifikasi'
 import { Link } from 'react-router-dom'
@@ -14,182 +14,181 @@ export function CetakBuktiPendaftaran({
   profil: PendaftarType
   jenjang: string
 }) {
-  const ref = useRef<HTMLDivElement>(null)
-  const [dataSiap, setDataSiap] = useState(false)
+  const contentToPrint = useRef(null)
 
-  useEffect(() => {
-    if (profil && jenjang) {
-      setDataSiap(true)
-    }
-  }, [profil, jenjang])
+  const handlePrint = useReactToPrint({
+    documentTitle: 'Print This Document',
+    onBeforePrint: async () => {
+      console.log('Loading fonts before printing...')
+      // Load the fonts
+      await document.fonts.ready
+      console.log('Fonts loaded')
+    },
+    onAfterPrint: () => console.log('after printing...'),
+    removeAfterPrint: true,
+  })
 
   return (
     <>
-      {dataSiap && (
-        <ReactToPrint
-          bodyClass="print-agreement"
-          content={() => ref.current}
-          trigger={() => (
-            <div className="flex items-center gap-12 hover:cursor-pointer">
-              <p className="text-nowrap rounded-full bg-slate-200 px-24 py-12 text-[2rem] text-slate-700 phones:text-[2.4rem]">
-                Bukti Daftar
-              </p>
-            </div>
-          )}
-        />
-      )}
-      {dataSiap && (
-        <section
-          className="absolute left-[-10000px] top-auto h-auto overflow-hidden"
-          aria-hidden
-          tabIndex={-1}
-        >
-          <div ref={ref}>
-            <div className="flex flex-col gap-32 p-32">
-              {/* --- Kop Surat --- */}
-              <div className="flex items-center gap-24 border-b-2 border-black-100 p-24">
-                <img src="/img/tutwuri.png" alt="PPDB" className="w-[15rem]" />
-                <div className="flex flex-col items-center gap-12">
-                  <p className="text-[5rem] font-bold uppercase">
-                    Pemerintah Kabupaten Batu Bara
-                  </p>
-                  <p className="text-[5rem] font-bold uppercase">
-                    dinas pendidikan
-                  </p>
-                  <p className="text-center text-[3rem]">
-                    Penerimaan Peserta Didik Baru (PPDB) Kabupaten Batubara
-                    Tahun 2024
-                  </p>
-                </div>
-              </div>
-              {/* --- Content Header --- */}
-              <div className="flex flex-col items-center gap-12 text-[4rem] font-bold uppercase">
-                <p>Bukti Pendaftaran</p>
-                <p className="text-center">
-                  Jenjang {jenjang?.toUpperCase()} PPDB Online Tahun Pelajaran
-                  2024/2025
+      <section
+        className="absolute left-[-10000px] top-auto h-auto overflow-hidden"
+        aria-hidden
+        tabIndex={-1}
+      >
+        <div ref={contentToPrint}>
+          <div className="flex flex-col gap-32 p-32">
+            {/* --- Kop Surat --- */}
+            <div className="flex items-center gap-24 border-b-2 border-black-100 p-24">
+              <img src="/img/tutwuri.png" alt="PPDB" className="w-[15rem]" />
+              <div className="flex flex-col items-center gap-12">
+                <p className="text-[5rem] font-bold uppercase">
+                  Pemerintah Kabupaten Batu Bara
+                </p>
+                <p className="text-[5rem] font-bold uppercase">
+                  dinas pendidikan
+                </p>
+                <p className="text-center text-[3rem]">
+                  Penerimaan Peserta Didik Baru (PPDB) Kabupaten Batubara Tahun
+                  2024
                 </p>
               </div>
-              {/* --- Divider --- */}
-              <p className="bg-background p-24 text-[3rem] font-bold">
-                Biodata Calon Peserta Didik
+            </div>
+            {/* --- Content Header --- */}
+            <div className="flex flex-col items-center gap-12 text-[4rem] font-bold uppercase">
+              <p>Bukti Pendaftaran</p>
+              <p className="text-center">
+                Jenjang {jenjang?.toUpperCase()} PPDB Online Tahun Pelajaran
+                2024/2025
               </p>
-              {/* --- Data Pendaftar --- */}
-              <div className="flex gap-32 text-[2.4rem]">
-                <div className="flex flex-1 flex-col gap-12">
+            </div>
+            {/* --- Divider --- */}
+            <p className="bg-background p-24 text-[3rem] font-bold">
+              Biodata Calon Peserta Didik
+            </p>
+            {/* --- Data Pendaftar --- */}
+            <div className="flex gap-32 text-[2.4rem]">
+              <div className="flex flex-1 flex-col gap-12">
+                <DataComponent2
+                  label="No. Pendaftar"
+                  value={profil?.nompes ?? '-'}
+                />
+                <DataComponent2 label="NIK" value={profil?.nik ?? '-'} />
+                {jenjang?.toLowerCase() === 'smp' && (
+                  <DataComponent2 label="NISN" value={profil?.nisn ?? '-'} />
+                )}
+                <DataComponent2
+                  label="Nama Lengkap"
+                  value={profil?.nama ?? '-'}
+                />
+                <DataComponent2
+                  label="Tempat/Tgl. Lahir"
+                  value={`${profil?.tempat_lahir}, ${dayjs(profil?.tanggal_lahir).format('DD MMMM YYYY')}`}
+                />
+                <DataComponent2
+                  label="Jenis Kelamin"
+                  value={
+                    profil?.jenis_kelamin === 'L'
+                      ? 'Laki-laki'
+                      : 'Perempuan' ?? '-'
+                  }
+                />
+                {jenjang?.toLowerCase() === 'smp' && (
                   <DataComponent2
-                    label="No. Pendaftar"
-                    value={profil?.nompes ?? '-'}
+                    label="Asal Sekolah"
+                    value={profil?.nama_sekolah ?? '-'}
                   />
-                  <DataComponent2 label="NIK" value={profil?.nik ?? '-'} />
-                  {jenjang?.toLowerCase() === 'smp' && (
-                    <DataComponent2 label="NISN" value={profil?.nisn ?? '-'} />
-                  )}
-                  <DataComponent2
-                    label="Nama Lengkap"
-                    value={profil?.nama ?? '-'}
-                  />
-                  <DataComponent2
-                    label="Tempat/Tgl. Lahir"
-                    value={`${profil?.tempat_lahir}, ${dayjs(profil?.tanggal_lahir).format('DD MMMM YYYY')}`}
-                  />
-                  <DataComponent2
-                    label="Jenis Kelamin"
-                    value={
-                      profil?.jenis_kelamin === 'L'
-                        ? 'Laki-laki'
-                        : 'Perempuan' ?? '-'
-                    }
-                  />
-                  {jenjang?.toLowerCase() === 'smp' && (
-                    <DataComponent2
-                      label="Asal Sekolah"
-                      value={profil?.nama_sekolah ?? '-'}
-                    />
-                  )}
+                )}
 
+                <DataComponent2
+                  label="Alamat"
+                  value={profil?.alamat_lengkap ?? '-'}
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-12">
+                <DataComponent2
+                  label="Jalur Pendaftaran"
+                  value={
+                    profil?.jalur === enumJalur.ZONASI
+                      ? 'Zonasi'
+                      : profil?.jalur === enumJalur.AFIRMASI
+                        ? 'Afirmasi'
+                        : profil?.jalur === enumJalur.PRESTASI
+                          ? 'Prestasi'
+                          : profil?.jalur === enumJalur.PINDAHTUGAS
+                            ? 'Pindah Tugas'
+                            : 'Zonasi'
+                  }
+                />
+                <DataComponent2
+                  label="Pilihan 1"
+                  value={
+                    profil?.pilihan1
+                      ? `${profil?.pilihan1} (Skor Sementara: ${profil?.skor1})`
+                      : '-'
+                  }
+                />
+                {jenjang?.toLowerCase() === 'smp' && (
                   <DataComponent2
-                    label="Alamat"
-                    value={profil?.alamat_lengkap ?? '-'}
-                  />
-                </div>
-                <div className="flex flex-1 flex-col gap-12">
-                  <DataComponent2
-                    label="Jalur Pendaftaran"
+                    label="Pilihan 2"
                     value={
-                      profil?.jalur === enumJalur.ZONASI
-                        ? 'Zonasi'
-                        : profil?.jalur === enumJalur.AFIRMASI
-                          ? 'Afirmasi'
-                          : profil?.jalur === enumJalur.PRESTASI
-                            ? 'Prestasi'
-                            : profil?.jalur === enumJalur.PINDAHTUGAS
-                              ? 'Pindah Tugas'
-                              : 'Zonasi'
-                    }
-                  />
-                  <DataComponent2
-                    label="Pilihan 1"
-                    value={
-                      profil?.pilihan1
-                        ? `${profil?.pilihan1} (Skor Sementara: ${profil?.skor1})`
+                      profil?.pilihan2
+                        ? `${profil?.pilihan2} (Skor Sementara: ${profil?.skor2})`
                         : '-'
                     }
                   />
-                  {jenjang?.toLowerCase() === 'smp' && (
-                    <DataComponent2
-                      label="Pilihan 2"
-                      value={
-                        profil?.pilihan2
-                          ? `${profil?.pilihan2} (Skor Sementara: ${profil?.skor2})`
-                          : '-'
-                      }
-                    />
-                  )}
-                  <DataComponent2
-                    label="Tanggal Pendaftaran"
-                    value={
-                      dayjs(profil?.daftar)
-                        .locale('id')
-                        .format('DD MMMM YYYY HH:mm:ss') ?? '-'
-                    }
-                  />
-                </div>
+                )}
+                <DataComponent2
+                  label="Tanggal Pendaftaran"
+                  value={
+                    dayjs(profil?.daftar)
+                      .locale('id')
+                      .format('DD MMMM YYYY HH:mm:ss') ?? '-'
+                  }
+                />
               </div>
-              {/* --- Divider --- */}
-              <p className="bg-background p-24 text-[3rem] font-bold">
-                Status Verifikasi Sekolah:{' '}
-                {Number(profil?.verifikasi) <= enumVerifikasi?.DIPROSES
-                  ? 'Menunggu Verifikasi dari salah satu sekolah pilihan anda!'
-                  : Number(profil?.verifikasi) === enumVerifikasi?.DISETUJUI
-                    ? 'Data anda sudah di verifikasi'
-                    : Number(profil?.verifikasi) === enumVerifikasi?.DITOLAK
-                      ? 'Verifikasi data anda ditolak'
-                      : 'Menunggu Verifikasi dari salah satu sekolah pilihan anda!'}
+            </div>
+            {/* --- Divider --- */}
+            <p className="bg-background p-24 text-[3rem] font-bold">
+              Status Verifikasi Sekolah:{' '}
+              {Number(profil?.verifikasi) <= enumVerifikasi?.DIPROSES
+                ? 'Menunggu Verifikasi dari salah satu sekolah pilihan anda!'
+                : Number(profil?.verifikasi) === enumVerifikasi?.DISETUJUI
+                  ? 'Data anda sudah di verifikasi'
+                  : Number(profil?.verifikasi) === enumVerifikasi?.DITOLAK
+                    ? 'Verifikasi data anda ditolak'
+                    : 'Menunggu Verifikasi dari salah satu sekolah pilihan anda!'}
+            </p>
+            <div className="flex flex-col gap-12 text-[2.4rem]">
+              <p>
+                *Tanggal Cetak{' '}
+                {dayjs().locale('id').format('DD MMMM YYYY hh:mm:ss A')}
               </p>
-              <div className="flex flex-col gap-12 text-[2.4rem]">
-                <p>
-                  *Tanggal Cetak{' '}
-                  {dayjs().locale('id').format('DD MMMM YYYY hh:mm:ss A')}
-                </p>
-                <div>
-                  * Untuk mendapatkan informasi lebih lanjut, silahkan kunjungi
-                  website dinas pendidikan{' '}
-                  <Link to="https://disdik.batubarakab.go.id">
-                    disdik.batubarakab.go.id
-                  </Link>
-                </div>
-                <div>
-                  * Atau kunjungi website resmi PPDB Kabupaten Batu Bara
-                  <Link to="https://ppdbonline-batubara.com">
-                    https://ppdbonline-batubara.com
-                  </Link>
-                </div>
+              <div>
+                * Untuk mendapatkan informasi lebih lanjut, silahkan kunjungi
+                website dinas pendidikan{' '}
+                <Link to="https://disdik.batubarakab.go.id">
+                  disdik.batubarakab.go.id
+                </Link>
+              </div>
+              <div>
+                * Atau kunjungi website resmi PPDB Kabupaten Batu Bara
+                <Link to="https://ppdbonline-batubara.com">
+                  https://ppdbonline-batubara.com
+                </Link>
               </div>
             </div>
           </div>
-        </section>
-      )}
+        </div>
+      </section>
+
+      <button
+        onClick={() => {
+          handlePrint(null, () => contentToPrint.current)
+        }}
+        className="text-nowrap text-[2rem] hover:text-danger-100 phones:text-[2.4rem]"
+      >
+        Bukti Daftar
+      </button>
     </>
   )
 }
--- src/pages/data-pendaftar/mapping-pendaftar.tsx
+++ src/pages/data-pendaftar/mapping-pendaftar.tsx
@@ -2,8 +2,6 @@ import Loading from '@/components/Loading'
 import { NoData } from '@/components/NoData'
 import { PendaftarType } from '@/libs/types/pendaftar-type'
 import dayjs from 'dayjs'
-import { DataComponent } from './data-component'
-import { ChevronDown, ChevronUp } from 'lucide-react'
 import { useState } from 'react'
 import React from 'react'
 import { ExpandData } from './expand-data'
@@ -25,27 +23,29 @@ export function MappingDataPendaftar({
     setExpandedRowIndex(expandedRowIndex === index ? null : index)
   }
 
+  const handlePrintClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event.stopPropagation()
+  }
+
   return (
     <div className="flex w-full flex-col gap-12 overflow-x-auto">
       <table className="scrollbar h-full w-full border-collapse overflow-auto text-[2rem] phones:text-[2.4rem]">
         <thead className="relative z-10 w-full align-top leading-medium">
           <tr className="w-full border-b-[1.6rem] border-transparent">
             <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase">
-              No
+              #
             </th>
             <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase">
-              Biodata
+              Nama/Tgl Lahir/NISN/NIK/Daftar
             </th>
             <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase">
-              Tujuan Sekolah
-            </th>
-            <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase">
-              Verifikasi
+              Pilihan
             </th>
             <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase">
               Cetak
             </th>
-            <th className="sticky top-0 border-b-2 bg-background p-4 px-24 py-12 text-left uppercase"></th>
           </tr>
         </thead>
         <tbody>
@@ -71,62 +71,64 @@ export function MappingDataPendaftar({
             data?.map((item, idx) => (
               <React.Fragment key={idx}>
                 <tr
-                  className="scrollbar h-full w-full overflow-auto border-b-[1.6rem] border-transparent transition-all ease-in odd:bg-surface-disabled hover:cursor-pointer hover:bg-warning-tint-2"
+                  className="scrollbar h-full w-full overflow-auto border-b border-black transition-all ease-in odd:bg-surface-disabled hover:cursor-pointer hover:bg-warning-tint-2"
                   onClick={() => toggleRow(idx)}
                 >
                   <td className="px-24 py-12 align-top leading-medium">
                     {idx + 1}
                   </td>
                   <td className="text-nowrap px-24 py-12 align-top leading-medium">
-                    <div className="flex flex-col gap-4">
-                      <DataComponent label="Nama" value={item?.nama ?? '-'} />
-                      <DataComponent
-                        label="TTL"
-                        value={
-                          `${item?.tempat_lahir}, ${dayjs(item?.tanggal_lahir)
+                    <div className="flex items-start gap-24">
+                      <div className="h-[10rem] w-[10rem]">
+                        <img src="/img/tutwuri.png" className="h-full w-full" />
+                      </div>
+                      <div className="flex flex-col gap-4">
+                        <p>{item?.nama ?? '-'}</p>
+                        <p>
+                          {item?.nisn
+                            ? `NISN:${item?.nisn}/NIK:${item?.nik}`
+                            : `NIK:${item?.nik}`}
+                        </p>
+                        <p className="italic text-danger-100">
+                          Daftar{' '}
+                          {dayjs(item?.daftar)
                             .locale('id')
-                            .format('DD MMMM YYYY')}` ?? '-'
-                        }
-                      />
-                      {item?.nisn && (
-                        <DataComponent label="NISN" value={item?.nisn ?? '-'} />
-                      )}
-                      <DataComponent label="NIK" value={item?.nik ?? '-'} />
-                      <DataComponent
-                        label="Daftar"
-                        value={dayjs(item?.daftar)
-                          .locale('id')
-                          .format('DD MMMM YYYY HH:mm:ss')}
-                      />
+                            .format('DD-MM-YYYY HH:mm:ss')}
+                        </p>
+                        <p className="italic text-danger-100">
+                          Diverifikasi Sekolah{' '}
+                          {dayjs(item?.verifikasi_on)
+                            .locale('id')
+                            .format('DD-MM-YYYY HH:mm:ss')}
+                        </p>
+                      </div>
                     </div>
                   </td>
                   <td className="px-24 py-12 align-top leading-medium">
                     <div className="flex flex-col gap-4">
                       <p className="text-nowrap">
-                        Pilihan 1 - {item?.pilihan1 ?? '-'} (Skor:{' '}
-                        {item?.skor1 ?? '0'})
+                        Pilihan 1 - {item?.pilihan1 ?? '-'} (Skore:{' '}
+                        {Math.round(Number(item?.skor1)) ?? '0'})
                       </p>
                       <p className="text-nowrap">
-                        Pilihan 2 - {item?.pilihan2 ?? '-'} (Skor:{' '}
-                        {item?.skor2 ?? '0'})
+                        Pilihan 2 - {item?.pilihan2 ?? '-'} (Skore:{' '}
+                        {Math.round(Number(item?.skor2)) ?? '0'})
                       </p>
                     </div>
                   </td>
+
                   <td className="px-24 py-12 align-top leading-medium">
-                    <div className="flex flex-col gap-4">
-                      <p>{item?.verifikasi_sekolah}</p>
-                      <p>{item?.verifikasi_user}</p>
-                      <p>{item?.verifikasi_on}</p>
+                    <div className="flex flex-col items-start justify-start gap-12">
+                      <button onClick={handlePrintClick}>
+                        <CetakBuktiPendaftaran
+                          profil={item}
+                          jenjang={jenjang}
+                        />
+                      </button>
+                      <button onClick={handlePrintClick}>
+                        <CetakHasilVerifikasi profil={item} jenjang={jenjang} />
+                      </button>
                     </div>
-                  </td>
-                  <td className="align-top">
-                    <div className="flex flex-col gap-12">
-                      <CetakBuktiPendaftaran profil={item} jenjang={jenjang} />
-                      <CetakHasilVerifikasi profil={item} jenjang={jenjang} />
-                    </div>
-                  </td>
-                  <td className="align-top">
-                    {expandedRowIndex === idx ? <ChevronUp /> : <ChevronDown />}
                   </td>
                 </tr>
                 {expandedRowIndex === idx && (
