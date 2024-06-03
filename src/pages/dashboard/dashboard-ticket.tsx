86a914bf44c7c0edfa17a8adcc9bd653e7450716
hexavhobia <rvx9forces@gmail.com>
2024-06-03 11:31:07 +0700

    fix: memperbaiki dashboard ticket

--- src/pages/dashboard/dashboard-ticket.tsx
+++ src/pages/dashboard/dashboard-ticket.tsx
@@ -1,13 +1,19 @@
 import Loading from '@/components/Loading'
 import { NoData } from '@/components/NoData'
-import { TiketType } from '@/libs/types/tiket-type'
+import { TiketSekolahType, TiketType } from '@/libs/types/tiket-type'
 import { useGetTiketQuery } from '@/store/slices/pertanyaanAPI'
+import { useGetTiketSekolahQuery } from '@/store/slices/tiketSekolahAPI'
+import clsx from 'clsx'
 import { Mail } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export function DashboardTiket() {
   const [listTiket, setListTiket] = useState<TiketType[]>([])
+  const [listTiketSekolah, setListTiketSekolah] = useState<TiketSekolahType[]>(
+    [],
+  )
+  const [menu, setMenu] = useState<string>('Siswa')
 
   const { data, isLoading, isFetching } = useGetTiketQuery({
     page: 1,
@@ -24,39 +30,117 @@ export function DashboardTiket() {
   }, [data])
   const navigate = useNavigate()
 
+  const {
+    data: sekolah,
+    isLoading: sekolahLoading,
+    isFetching: sekolahFetching,
+  } = useGetTiketSekolahQuery({
+    page: 1,
+    page_size: 5,
+    status: -1,
+    search: '',
+    id_masalah: '',
+  })
+  const loadingSekolah = sekolahFetching || sekolahLoading
+
+  useEffect(() => {
+    if (sekolah) {
+      setListTiketSekolah(sekolah?.data)
+    }
+  }, [sekolah])
+
   return (
     <div className="flex flex-col gap-64">
       <div className="flex justify-between gap-32 text-[#005479]">
         <p className="text-[3.2rem] font-bold">Ticket Terbaru</p>
-        <Link to="/open-ticket" className="underline hover:text-primary">
-          Lihat Semua
-        </Link>
+        {menu === 'Siswa' ? (
+          <Link to="/open-ticket" className="underline hover:text-primary">
+            Lihat Semua
+          </Link>
+        ) : (
+          <Link
+            to="/open-ticket/sekolah"
+            className="underline hover:text-primary"
+          >
+            Lihat Semua
+          </Link>
+        )}
       </div>
       {/* --- Data --- */}
-      {loading ? (
-        <Loading />
-      ) : listTiket?.length > 0 ? (
-        <div className="flex flex-col">
-          {listTiket?.slice(0, 5).map((item, idx) => (
-            <div
-              className="flex items-center gap-32 border-b border-[#e0e4e5] py-24 hover:cursor-pointer hover:bg-yellow-50"
-              key={idx}
-              onClick={() => {
-                navigate(`/open-ticket?detail=${item?.id}`)
-              }}
-            >
-              <div className="limited-text-2-lines flex-1 overflow-hidden">
-                <div dangerouslySetInnerHTML={{ __html: item?.keterangan }} />
+      <div className="flex flex-col gap-32">
+        <div className="flex">
+          <div className="flex items-center gap-32">
+            {['Siswa', 'Admin'].map((item, idx) => (
+              <div
+                className={clsx(
+                  'border-b pb-8 hover:cursor-pointer hover:text-[#005479]',
+                  {
+                    ' border-[#005479] text-[#005479]': item === menu,
+                    ' border-transparent': item !== menu,
+                  },
+                )}
+                key={idx}
+                onClick={() => {
+                  setMenu(item)
+                }}
+              >
+                {item}
               </div>
-              <span className="rounded-2xl bg-yellow-500 p-8 text-white">
-                <Mail size={16} />
-              </span>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      ) : (
-        <NoData />
-      )}
+        {menu === 'Siswa' ? (
+          loading ? (
+            <Loading />
+          ) : listTiket?.length > 0 ? (
+            <div className="flex flex-col">
+              {listTiket?.slice(0, 5).map((item, idx) => (
+                <div
+                  className="flex items-center gap-32 border-b border-[#e0e4e5] py-24 hover:cursor-pointer hover:bg-yellow-50"
+                  key={idx}
+                  onClick={() => {
+                    navigate(`/open-ticket?detail=${item?.id}`)
+                  }}
+                >
+                  <div className="limited-text-2-lines flex-1 overflow-hidden">
+                    <div
+                      dangerouslySetInnerHTML={{ __html: item?.keterangan }}
+                    />
+                  </div>
+                  <span className="rounded-2xl bg-yellow-500 p-8 text-white">
+                    <Mail size={16} />
+                  </span>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <NoData />
+          )
+        ) : loadingSekolah ? (
+          <Loading />
+        ) : listTiketSekolah?.length > 0 ? (
+          <div className="flex flex-col">
+            {listTiketSekolah?.slice(0, 5).map((item, idx) => (
+              <div
+                className="flex items-center gap-32 border-b border-[#e0e4e5] py-24 hover:cursor-pointer hover:bg-yellow-50"
+                key={idx}
+                onClick={() => {
+                  navigate(`/open-ticket?detail=${item?.id}`)
+                }}
+              >
+                <div className="limited-text-2-lines flex-1 overflow-hidden">
+                  <div dangerouslySetInnerHTML={{ __html: item?.keterangan }} />
+                </div>
+                <span className="rounded-2xl bg-yellow-500 p-8 text-white">
+                  <Mail size={16} />
+                </span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <NoData />
+        )}
+      </div>
     </div>
   )
 }
